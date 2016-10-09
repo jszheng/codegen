42f29dcdfdb7dd316fa77aa56b47959261be05e6
Jinshan Zheng <zheng_js@hotmail.com>
2016-10-09 17:01:01 +0800

    initial check-in

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # codegen
 combined tools for code generation
+
+## modules
+* [pyverilog](https://github.com/PyHDI/Pyverilog)
+* [veriloggen](https://github.com/PyHDI/veriloggen)
+* ANTLR4 Verilog parser for python
+* Meta HDL
---
+++ antlr4_verilog/__init__.py
@@ -0,0 +1,4 @@
+from antlr4_verilog.Verilog2001Lexer import Verilog2001Lexer
+from antlr4_verilog.Verilog2001Parser import Verilog2001Parser
+from antlr4_verilog.Verilog2001Listener import Verilog2001Listener
+from antlr4_verilog.Verilog2001Visitor import Verilog2001Visitor
